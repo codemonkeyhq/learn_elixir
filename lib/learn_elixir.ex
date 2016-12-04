ca539a3e6765624e930897e0e7127414415e05d9
root <jennifermorgan2016>
2016-12-04 13:04:38 -0500

    Individaul trings working

--- lib/learn_elixir.ex
+++ lib/learn_elixir.ex
@@ -1,2 +1,18 @@
 defmodule LearnElixir do
+
+
+  def count_up_down(n) do
+    ans = count_up_down(n-1,"")    
+    String.reverse(ans) <> "#{n}" <> ans
+  end
+
+  defp count_up_down(0,acc) do
+    acc
+  end
+
+  defp count_up_down(n, acc) do
+    count_up_down(n-1, acc <> "#{n}")
+  end
+
+
 end
--- test/learn_elixir_test.exs
+++ test/learn_elixir_test.exs
@@ -1,8 +1,18 @@
 defmodule LearnElixirTest do
   use ExUnit.Case
   doctest LearnElixir
+  # import out code library
+  import LearnElixir
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "check we can return 1" do
+    assert count_up_down(1)  == "1"
+  end
+
+  test "check out for number 2 is '121'" do
+    assert count_up_down(2) == "121"
+  end
+
+  test "we can go all the way to 9" do
+    assert count_up_down(9) == "12345678987654321"
   end
 end
